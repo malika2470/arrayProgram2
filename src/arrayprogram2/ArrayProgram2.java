8d4f266157d130b8dbc90f86f103dda9f44bd96e
malika2470 <55253110+malika2470@users.noreply.github.com>
2019-10-18 13:27:57 -0400

    Update ArrayProgram2.java

--- src/arrayprogram2/ArrayProgram2.java
+++ src/arrayprogram2/ArrayProgram2.java
@@ -1,7 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/*Malika Sharma
+ *18/10/2019
+ *Array Programs 2
+ * 
  */
 
 package arrayprogram2;
